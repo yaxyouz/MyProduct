d1f664008ad7169923985b5d7768935e73b46494
yaxyouz <sodiqjonovbot@gmail.com>
2022-03-09 13:37:59 +0500

    add freez

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const nightDay = document.querySelector(".header__btn-toggle");
 const body = document.querySelector("body");
 
 btn.addEventListener("click", function () {
-  btnOpened.classList.toggle("header__btn--opened")
+  btnOpened.classList.toggle("header__btn--opened");
 })
   nightDay.addEventListener("click", function () {
   body.classList.toggle("night-day")
@@ -17,9 +17,11 @@ const navigation = document.querySelector(".header__nav");
 
 burgerToggler.addEventListener("click", function() {
     if (!burgerToggler.classList.contains("header__burger--opened")) {
+      body.classList.add("freez");
       burgerToggler.classList.add("header__burger--opened");
       navigation.classList.add("header__nav--opened");
     } else {
+      body.classList.remove("freez");
       burgerToggler.classList.remove("header__burger--opened");
       navigation.classList.add("header__nav--closing");
     }
